14230c0f569d27a318a785ac7cd679aeef54592e
Juni Yadi <juniyadi94@gmail.com>
2019-11-19 21:49:31 +0700

    Upadate Example

--- example/sendgroup.php
+++ example/sendgroup.php
@@ -8,12 +8,7 @@ use PremiumFastNetwork\WSGateway;
 $wa = new WSGateway();
 $wa->token($token);
 $wa->deviceid($deviceid);
-
-// send message for single number
-$send = $wa->sendmessage($number, 'test send message by api');
-
-// send message for multiple number
-// $send = $wa->sendmessage($multiplenumber, 'test send message by api');
+$send = $wa->sendgroup('xxxxx', 'test send message by api');
 
 // return output
 var_dump(json_decode($send));
