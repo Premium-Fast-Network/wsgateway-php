cfd721585f99f3495ced05711ae6b9996e87db0d
Juni Yadi <juniyadi94@gmail.com>
2019-11-19 21:15:21 +0700

    Update Example For Bulk Message

--- example/sendmessage.php
+++ example/sendmessage.php
@@ -8,5 +8,12 @@ use PremiumFastNetwork\WSGateway;
 $wa = new WSGateway();
 $wa->token($token);
 $wa->deviceid($deviceid);
+
+// send message for single number
 $send = $wa->sendmessage($number, 'test send message by api');
+
+// send message for multiple number
+$send = $wa->sendmessage($multiplenumber, 'test send message by api');
+
+// return output
 var_dump(json_decode($send));
